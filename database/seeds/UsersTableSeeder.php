accb6b06674bbd032e1da6ebdb9cc1a841ca8bb8
Alice Ni <niming0522@gmail.com>
2019-09-07 16:23:23 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Alice';
         $user->email = 'aliceni@astri.org';
+        $user->is_admin = true;
         $user->save();
     }
 }
